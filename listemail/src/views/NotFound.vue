92961596d1073223e3f201e4b781eab57d1978df
Adebisi_Ponmile <adebisiponmile@gmail.com>
2024-06-11 18:18:13 +0100

    notfound page link

--- listemail/src/views/NotFound.vue
+++ listemail/src/views/NotFound.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <p>Page Not Found please kindly navigate back</p>
+    <div class="m-auto text-center relative h-full flex flex-col justify-center">
+        <p class="m-auto">Page Not Found please kindly navigate back
+            <span class="text-white"><router-link to="/">home</router-link></span>
+        </p>
     </div>
 </template>
 
