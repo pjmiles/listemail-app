749b467f30b3c410da5658dc3df2ce00e29a74d1
Adebisi_Ponmile <adebisiponmile@gmail.com>
2024-06-11 18:19:38 +0100

    path adjust

--- listemail/src/router/index.js
+++ listemail/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TableView from '../views/TableView.vue'
 import LoginViewVue from '@/views/LoginView.vue'
+import NotFoundVue from '@/views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,11 +12,16 @@ const router = createRouter({
       component: LoginViewVue
     },
     {
-      path: '/:id',
+      path: '/table',
       name: 'table',
       component: TableView,
       meta: {requireAuth: true}
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notfound',
+      component: NotFoundVue,
+    }
   ]
 })
 
