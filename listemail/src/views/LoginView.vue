1c29f6672e3c318eb16e3abf13cd6c17fbc47fca
Adebisi_Ponmile <adebisiponmile@gmail.com>
2024-06-13 13:42:27 +0100

    Login adjusted

--- listemail/src/views/LoginView.vue
+++ listemail/src/views/LoginView.vue
@@ -1,54 +1,56 @@
 
 <template>
-    <div class="flex items-center justify-center h-screen">
-        <form @submit.prevent="login" class="w-2/5 mx-auto bg-gray-800 p-6 rounded-lg">
-            <div class="h-2">
-                <p v-if="error" class="text-red-500 mt-2 text-center">{{ error }}</p>
-            </div>
-            <div class="mb-5">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                <input v-model="email" type="email" id="email" placeholder="admin@email.com" required class="w-full p-2" />
-            </div>
-            <div class="mb-5">
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input v-model="password" type="password" id="password" class="w-full p-2" required />
-            </div>
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 w-full p-2 mt-3">Submit</button>
-        </form>
-    </div>
+  <div class="flex items-center justify-center h-screen">
+    <form @submit.prevent="login" class="w-2/5 mx-auto bg-gray-800 p-6 rounded-lg">
+      <div class="h-10">
+        <p v-if="error" class="text-red-500 mt-2 text-center">{{ error }}</p>
+      </div>
+      <div class="mb-5">
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+        <input v-model="email" type="email" id="email" placeholder="admin@email.com" required class="w-full p-2" />
+      </div>
+      <div class="mb-5">
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+        <input v-model="password" type="password" id="password" class="w-full p-2" required />
+      </div>
+      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 w-full p-2 mt-3">Submit</button>
+    </form>
+  </div>
 </template>
 
   
 <script>
-import axios from 'axios';
 import axiosInstace from '../axios/axiosInstance';
 export default {
-    data() {
-        return {
-            email: '',
-            password: '',
-            error: ''
-        };
-    },
-    methods: {
-        async login() {
-            try {
-                const response = await axios.post('http://localhost:5173/login', {
-                    email: this.email,
-                    password: this.password
-                });
-                console.log('Login successful', response.data);
-                localStorage.setItem('token', response.data.token);
-                this.$router.push('/table');
-            } catch (error) {
-                if (error.response) {
-                    this.error = error.response.data.message;
-                } else {
-                    this.error = 'An error occurred. Please try again.';
-                }
-                console.error('Login error', error);
-            }
+  data() {
+    return {
+      email: 'abi@gridirontest.com',
+      password: 'password1',
+      error: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axiosInstace.post('/login', {
+          email: this.email,
+          password: this.password
+        });
+        if (response.status === 200) {
+          console.log('Login successful', response.data);
+          localStorage.setItem('token', response.data.token);
+          this.$router.push('/table');
         }
+      } catch (error) {
+        if (error.response) {
+          this.error = error.response.data.message;
+        } else {
+          this.error = 'An error occurred. Please try again.';
+        }
+        this.error = 'An error occurred. Please try again.';
+        console.error('Login error', error);
+      }
     }
+  }
 };
 </script>
