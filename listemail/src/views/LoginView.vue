8dd1e5c749949894edf92fefd57c1d8dcd0a5038
Adebisi_Ponmile <adebisiponmile@gmail.com>
2024-06-14 15:14:19 +0100

    height adjust for LoginPage

--- listemail/src/views/LoginView.vue
+++ listemail/src/views/LoginView.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="flex items-center justify-center h-screen">
+  <div class="flex items-center justify-center my-[100px]">
     <form @submit.prevent="login" class="w-2/5 mx-auto bg-gray-800 p-6 rounded-lg">
       <div class="h-10">
         <p v-if="error" class="text-red-500 mt-2 text-center">{{ error }}</p>
